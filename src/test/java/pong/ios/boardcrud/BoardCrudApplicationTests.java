b6b6d01518aa65c44d4734a4b81cefe361849066
hyuntroll <83080414+hyuntroll@users.noreply.github.com>
2025-09-12 20:44:14 +0900

    initial commit

---
+++ src/test/java/pong/ios/boardcrud/BoardCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package pong.ios.boardcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
