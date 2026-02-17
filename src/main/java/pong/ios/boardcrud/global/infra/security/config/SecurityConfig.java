011925ab7e95509d263ab64dc144f9766a4e74ee
hyuntroll <83080414+hyuntroll@users.noreply.github.com>
2026-02-18 02:13:08 +0900

    fix: permitAll sign-up uri

--- src/main/java/pong/ios/boardcrud/global/infra/security/config/SecurityConfig.java
+++ src/main/java/pong/ios/boardcrud/global/infra/security/config/SecurityConfig.java
@@ -63,6 +63,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests((auth) -> auth
                         .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/api/v1/user/sign-up").permitAll()
                         .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html").permitAll()
                         .anyRequest().authenticated()
                 );
