a9b2bf0139c36345951f3173f2a9020890cccb3c
hyuntroll <83080414+hyuntroll@users.noreply.github.com>
2026-02-18 01:28:29 +0900

    feat: persistenceAdapter implement

--- src/main/java/pong/ios/boardcrud/adapter/out/persistence/user/repository/UserRepository.java
+++ src/main/java/pong/ios/boardcrud/adapter/out/persistence/user/repository/UserRepository.java
@@ -3,5 +3,12 @@ package pong.ios.boardcrud.adapter.out.persistence.user.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pong.ios.boardcrud.adapter.out.persistence.user.entity.UserEntity;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    Optional<UserEntity> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+
+    boolean existsByUsername(String username);
 }
